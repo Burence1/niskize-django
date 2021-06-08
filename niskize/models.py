a655efee988dd1f65e385cefcded544db6a87bf3
Burence1 <burensomondi@gmail.com>
2021-06-09 00:09:39 +0300

    add user create profile instance

--- niskize/models.py
+++ niskize/models.py
@@ -2,18 +2,25 @@ from django.db import models
 from django.db.models.fields.related import OneToOneField
 from django.contrib.auth.models import User
 import uuid
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 # Create your models here.
 
 class Profile(models.Model):
-  user=models.OneToOneField(User,on_delete=models.CASCADE)
+  user=models.OneToOneField(User,on_delete=models.CASCADE, related_name='profile')
   name = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-
-
 
   def __str__(self):
     return self.user.username
   
-
+  @receiver(post_save, sender=User)
+  def create_user_profile(sender, instance, created, **kwargs):
+      if created:
+          Profile.objects.create(user=instance)
+  
+  @receiver(post_save, sender=User)
+  def save_user_profile(sender, instance, **kwargs):
+      instance.profile.save()
 
 class Articles(models.Model):
   title = models.CharField(max_length=120)
